812ae4cc69983e0f227c30a8d8ef9d7f648a096e
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2017-01-20 23:35:00 -0200

    Bug fix: unity version

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float m_MaxShooterForce = 3.0f;
+
+    private Vector2 m_LeftShooterForce;
+    private Vector2 m_RightShooterForce;
+
+    private Rigidbody2D m_Rigidbody2D;
+
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+	
+	private void Update ()
+    {
+
+
+        //Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //rb2d.AddForce(movement * speed);
+    }
+}
